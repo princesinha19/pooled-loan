e837a34464dc5e882d3bba6760011694a00b4901
Prince Sinha <sinhaprince013@gmail.com>
2020-10-21 12:55:56 +0530

    Updates

---
+++ precache-manifest.4b97efba2b6219f9139538c2937a07b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dbe23ddc078602c3aa78c505deb07d",
+    "url": "/pooled-loan/index.html"
+  },
+  {
+    "revision": "b686847b7f2911e9875b",
+    "url": "/pooled-loan/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9a7ea5d5074fa477c906",
+    "url": "/pooled-loan/static/css/main.fad53284.chunk.css"
+  },
+  {
+    "revision": "b686847b7f2911e9875b",
+    "url": "/pooled-loan/static/js/2.c2236159.chunk.js"
+  },
+  {
+    "revision": "ce4ad4d68a15b620cfdaa86fb04a1fd0",
+    "url": "/pooled-loan/static/js/2.c2236159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7ea5d5074fa477c906",
+    "url": "/pooled-loan/static/js/main.bd165bae.chunk.js"
+  },
+  {
+    "revision": "4e1d32ae64af7931de9f",
+    "url": "/pooled-loan/static/js/runtime-main.2f5e26e9.js"
+  },
+  {
+    "revision": "f0181479ea35d984e778408808136eb6",
+    "url": "/pooled-loan/static/media/0x.f0181479.png"
+  },
+  {
+    "revision": "2dcc77d9a617cbd2a61d6fc2b9d7bc9f",
+    "url": "/pooled-loan/static/media/aave.2dcc77d9.png"
+  },
+  {
+    "revision": "4b911f884392c479b48b07c5e6f70fca",
+    "url": "/pooled-loan/static/media/logo.4b911f88.png"
+  },
+  {
+    "revision": "e9ada72b93b9cdb343c1f49bfa413c29",
+    "url": "/pooled-loan/static/media/metamask.e9ada72b.png"
+  }
+]);
